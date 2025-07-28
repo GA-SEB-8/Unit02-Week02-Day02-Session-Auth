25426ab699ac0b3ef9d5e4a0eef39b2a38f29c36
omarakamal <omaraakamal@gmail.com>
2025-07-28 13:35:03 +0300

    update

--- class-code/routes/auth.routes.js
+++ class-code/routes/auth.routes.js
@@ -2,6 +2,18 @@ const router = require("express").Router()
 const User = require("../models/User")
 
 
+router.get("/sign-up",(req,res)=>{
+    res.render("auth/sign-up.ejs")
+})
 
+router.post("/sign-up",async(req,res)=>{
+    try{
+        await User.create(req.body)
+        res.redirect("/auth/sign-up")
+    }
+    catch(error){
+        console.log(error)
+    }
+})
 
 module.exports = router
