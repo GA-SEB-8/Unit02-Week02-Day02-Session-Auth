172d26a9aa1ee72813aa93b478681198a2cfad1c
omarakamal <omaraakamal@gmail.com>
2025-07-28 13:48:30 +0300

    update

--- class-code/routes/auth.routes.js
+++ class-code/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 
 router.get("/sign-up",(req,res)=>{
@@ -8,12 +9,24 @@ router.get("/sign-up",(req,res)=>{
 
 router.post("/sign-up",async(req,res)=>{
     try{
+
+        
+        const hashedPassword = bcrypt.hashSync(req.body.password,10) // encrypts our password
+
+        req.body.password = hashedPassword
+
         await User.create(req.body)
+
         res.redirect("/auth/sign-up")
     }
     catch(error){
         console.log(error)
     }
+})
+
+
+router.get("/login",(req,res)=>{
+    res.render("auth/login.ejs")
 })
 
 module.exports = router
